9da581106920b30864c4867a611b56e9b3af7e4a
LeGBT <botepobel@gmail.com>
2011-04-25 13:01:42 +0200

    prod modifs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,6 +29,7 @@ Distorrent::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
+  #config.serve_static_assets = true
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
--- config/initializers/daemon.rb
+++ config/initializers/daemon.rb
@@ -3,6 +3,8 @@ require "#{Rails.root}/app/distorrent/parsexml"
 require "#{Rails.root}/app/distorrent/tlogger"
 require "Hpricot" #looks like this line is needed because of the threading in homecontroler
 
+  	HomeController.new.scan
+
 scheduler = Rufus::Scheduler.start_new
 
 scheduler.every '13m' do
